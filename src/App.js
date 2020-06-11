8152fc488d029e6d2128e776baab59f1f78ac153
Hamza Hashim <58355851+hamzahashim011@users.noreply.github.com>
2020-06-11 17:14:14 +0500

    Update App.js
    update app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello from App.js by hamza
+        hello world
       </header>
     </div>
   );
